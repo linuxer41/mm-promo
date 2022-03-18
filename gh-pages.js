77a1d2af7fbc7cace3d4f9215d640e4d38065cf3
Francisco Gonzales <linuxer42@gmail.com>
2022-03-17 23:44:46 -0400

    some

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ publish(
  'build', // path to public directory
  {
   branch: 'gh-pages',
-  repo: 'https://linuxer41.github.io/mm-promo.git', // Update to point to your repository
+  repo: 'https://github.com/linuxer41/mm-promo.git', // Update to point to your repository
   user: {
    name: 'francisco ochoa', // update to use your name
    email: 'linuxer41@gmail.com' // Update to use your email
